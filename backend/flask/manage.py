6eb9c98832b20e0ee7e327cfe50b4572fb2ca551
overwang <wangshunhong@mail.ustc.edu.cn>
2022-07-02 02:23:32 -0700

    leave the frontend

--- backend/flask/manage.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from app import app
-from models import db
-
-if __name__ == "__main__":
-    db.drop_all()
-    # db.create_all()
-    app.run(host='localhost',port='5000',debug=True)
