22aced68cbb0211d55743c007f9fd192b3bda55e
overwang <wangshunhong@mail.ustc.edu.cn>
2022-07-05 23:25:10 -0700

    subtree.sh

--- subtree.sh
+++ subtree.sh
@@ -5,5 +5,5 @@ git remote add github.io https://github.com/sonic-car/sonic-car.github.io.git
 
 rm -r pages
 
-git subtree add --prefix=pages subtree-origin main --squash
+git subtree add --prefix=pages gituhub.io main --squash
 
